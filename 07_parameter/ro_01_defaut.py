19c5c68df2b154e349fddb5b816124adc25369bd
roingbu <769485059@qq.com>
2020-06-02 11:21:06 +0800

    re

--- 07_parameter/ro_01_defaut.py
+++ 07_parameter/ro_01_defaut.py
@@ -4,11 +4,11 @@
 @Author: Roing
 @Date: 2020-06-01 21:08:32
 @LastEditors: Roing
-@LastEditTime: 2020-06-01 21:13:13
+@LastEditTime: 2020-06-02 11:05:13
 '''
 def sex(gender=True):
     
-    sex_inf = "man"
+    sex_inf = "man" 
     if not gender:
         sex_inf = "girl"
     
---
+++ 07_parameter/ro_02_multivalued_parameter.py
@@ -0,0 +1,18 @@
+'''
+@Descripttion: 
+@version: 
+@Author: Roing
+@Date: 2020-06-02 11:07:47
+@LastEditors: Roing
+@LastEditTime: 2020-06-02 11:10:04
+'''
+def sum(*args,**kwargs):
+
+    n = 0
+    
+    if num in args:
+        n+=num
+    
+    return n
+
+number = sum(1,3,4,5,**7)
